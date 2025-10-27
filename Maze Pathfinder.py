f5eb2340b73d2e442f70eea8d7c07732b437f91b
Luke Gardner <lukegardner09@icloud.com>
2025-10-27 01:27:17 +0000

    Add files via upload

---
+++ Maze Pathfinder.py
@@ -0,0 +1,68 @@
+import time
+
+
+maze = [
+    list("#####################"),
+    list("#     #        #  E##"),
+    list("# ### # ###### # ## #"),
+    list("# #   #      # #    #"),
+    list("# # ###### # # #### #"),
+    list("# #        # #      #"),
+    list("# ########## ###### #"),
+    list("#        #         ##"),
+    list("###### # # ##########"),
+    list("#      # #         ##"),
+    list("# ###### ######### ##"),
+    list("#        #         ##"),
+    list("######## # ##########"),
+    list("#R                 ##"),
+    list("#####################")] # defines the maze as a 2d array so it is easily edited
+
+moves = ((0,1), (1,0), (0,-1), (-1,0)) #sets out every possible move the robot can take
+visited = set() #makes the variable visited a set so there is no duplicate variables in it
+
+def find(maze, item):					#finds the grid location of the start and end points
+    for i in range(len(maze)):			#repeats for every row and column until answer is found
+        for x in range(len(maze[i])):
+            if maze[i][x] == item:
+                return (i,x) 			#returns coordinates
+
+def allowedMove(row,col):
+    if row >= 0 and row < len(maze) and col >= 0 and col < len(maze[0]) and (row, col) not in visited and maze[row][col] != '#':	#checks the new column fits the parameters to be allowed(in the maze, not a # and not visited)
+        return True
+    else:
+        return False
+    
+    
+def dfs(row, col, visited):				#depth first search
+    if maze[row][col] == 'E':		#checks if the endpoint has been found yet 
+        return True	#goal found
+    visited.add((row,col))				#adds the current grid location to the set visited so it can be checked later
+    for move in moves:					#repeats for all 4 possible moves
+        newRow = row + move[0]			#starting from the 1st input in moves, increments the column and row by each possible move amount
+        newCol = col + move[1]			
+        if allowedMove(newRow,newCol):	#checks new location is possible
+            if dfs(newRow,newCol,visited):#if the move is possible recursively calls dsf from inside dsf allowing constant repetition until the solution is found
+                if (row,col) != startPoint:
+                    maze[row][col] = '.'#sets the correct path to . after recursion the path is found
+                return True				#if dsf is true returns true
+    return False # if no solution can be found
+            
+startPoint = (find(maze, 'R'))	#find the start and end
+endPoint = (find(maze, 'E'))
+
+for l in range(len(maze)):
+    print (*maze[l])
+time.sleep(1)
+print("\n")
+
+dfs(startPoint[0],startPoint[1],visited)	#calls dsf and brings in the starting column and row as well as the empty set visited
+for l in range(len(maze)):
+    print (*maze[l])
+    
+#Pros + Cons of Depth First Search
+    #PROS:						CONS
+    #Fast acting				Doesn't guarantee shortest path
+    #Easy to implement			Can cause stack overflow
+    #Flexible					Depends on the order of the moves when it is set up
+    
